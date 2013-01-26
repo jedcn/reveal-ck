f230ec6c8b94b7e7f56d70f28145ba8efd306e62
Jed Northridge <northridge@gmail.com>
2013-01-26 15:47:25 -0500

    [file-slicer] FileSlicer: It removes a numeric range of lines from a file

--- lib/reveal-ck/file_slicer.rb
+++ lib/reveal-ck/file_slicer.rb
@@ -1,7 +1,23 @@
 class FileSlicer
 
-  def self.slice!(file, range)
+  def self.slice!(path, range)
+    slice_file = if File.exists? path
+      path
+    else
+      File.expand_path(File.join(Dir.pwd, path))
+    end
+    lines = File.open(slice_file).readlines
+    slicer = FileSlicer.new lines
+    sliced_lines = slicer.remove range
+    File.open(slice_file, 'w') { |f| f << sliced_lines.join }
+  end
 
+  def initialize(lines)
+    @lines = lines
+  end
+
+  def remove(range)
+    @lines.select.with_index { |line, index| ! range.cover? index }
   end
 
 end
