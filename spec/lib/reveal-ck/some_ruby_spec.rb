fb69e80455247e197908f6c9927b2ec776a9536e
Jed Northridge <northridge@gmail.com>
2013-02-15 07:15:55 -0500

    [fooling-around] Added a spec for String.*!

--- spec/lib/reveal-ck/some_ruby_spec.rb
+++ spec/lib/reveal-ck/some_ruby_spec.rb
@@ -4,6 +4,12 @@ describe 'Array.zip' do
     chars = [ 'a', 'b' ]
 
     result = nums.zip chars
-    result.should == [ [ 1, 'a' ], [ 2, b ] ]
+    result.should == [ [ 1, 'a' ], [ 2, 'b' ] ]
+  end
+end
+
+describe 'String.*' do
+  it 'repeats a string n times' do
+    ('x' * 16).should == 'xxxxxxxxxxxxxxxx'
   end
 end
