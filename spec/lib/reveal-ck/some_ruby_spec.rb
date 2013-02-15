cfa66c98f4f9468914ad144a223429efd49093a7
Jed Northridge <northridge@gmail.com>
2013-02-15 07:08:44 -0500

    [fooling-around] a test of Array.zip

---
+++ spec/lib/reveal-ck/some_ruby_spec.rb
@@ -0,0 +1,9 @@
+describe 'Array.zip' do
+  it 'can join two arrays together like a zipper' do
+    nums = [ 1, 2 ]
+    chars = [ 'a', 'b' ]
+
+    result = nums.zip chars
+    result.should == [ [ 1, 'a' ], [ 2, b ] ]
+  end
+end
