477ea4498890ea424321f000372bb190f333a5ea
Jed Northridge <jnorthridge@constantcontact.com>
2013-07-07 15:50:54 -0400

    [builder] Broke SlideBuilder down into smaller tasks

--- lib/reveal-ck/slide_builder.rb
+++ lib/reveal-ck/slide_builder.rb
@@ -3,24 +3,48 @@ module RevealCK
 
     attr_reader :user_slides, :reveal_slides
 
+    attr_reader :tasks
+
     def initialize(args)
       @user_slides = args[:user_slides]
       @reveal_slides = args[:reveal_slides]
+      register_tasks
     end
 
     def build!
+      tasks.each {|task| task.go!}
+    end
 
-      line_nums = {
-        default_slides: {
-          first: 37,
-          last: 338
-        }
-      }
+    private
 
-      default_slides = line_nums[:default_slides][:first]..line_nums[:default_slides][:last]
-      RevealCK::FileSlicer.remove! reveal_slides, default_slides
-      RevealCK::FileSplicer.insert! user_slides, into: reveal_slides, after: '<div class="slides">'
+    def add_task(description, action)
+      @tasks << BuildTask.new({
+                                builder: self,
+                                description: description,
+                                action: action
+                              })
+    end
 
+    def register_tasks
+
+      @tasks = []
+
+      add_task "Slicing out reveal.js default slides",
+               lambda {
+                 line_nums = {
+                   default_slides: {
+                     first: 37,
+                     last: 338
+                   }
+                 }
+                 default_slides = line_nums[:default_slides][:first]..line_nums[:default_slides][:last]
+                 FileSlicer.remove! reveal_slides, default_slides
+               }
+
+      add_task "Splicing in slides from #{user_slides}",
+               lambda {
+                 FileSplicer.insert! user_slides, into: reveal_slides, after: '<div class="slides">'
+               }
     end
 
   end
