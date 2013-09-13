b17b6d93c15e825143ab923242e00a90147360b8
Jed Northridge <northridge@gmail.com>
2013-09-13 06:04:06 -0400

    [api] SlidesHtmlBuilder can create slides.html

---
+++ spec/lib/reveal-ck/slides_html_builder_spec.rb
@@ -0,0 +1,85 @@
+require 'spec_helper'
+require 'tempfile'
+
+module RevealCK
+  describe SlidesHtmlBuilder do
+
+    let :data do
+      File.join 'spec', 'data', 'slides_html_builder'
+    end
+
+    context 'when working with slim' do
+
+      let :input_file do
+        File.join data, 'slides.slim'
+      end
+
+      describe '#render' do
+        it 'knows how to create slide content from slim' do
+          builder = SlidesHtmlBuilder.new input_file: input_file
+          expect(builder.render).to include "<section>"
+          expect(builder.render).to include "<h1>"
+          expect(builder.render).to include "Hello Slim!"
+        end
+      end
+
+    end
+
+    context 'when working with haml' do
+
+      let :input_file do
+        File.join data, 'slides.haml'
+      end
+
+      describe '#render' do
+        it 'knows how to create slide content from haml' do
+          builder = SlidesHtmlBuilder.new input_file: input_file
+          expect(builder.render).to include "<section>"
+          expect(builder.render).to include "<h1>"
+          expect(builder.render).to include "Hello Haml!"
+        end
+      end
+
+    end
+
+    context 'when working with a Presentation' do
+
+      let :presentation do
+        quote = Slide.new({
+                            template: 'quote',
+                            quote:    'Hello Ruby!',
+                            citation: 'https://www.ruby-lang.org/en/about/'
+                          })
+        presentation = Presentation.new
+        presentation.add_slide quote
+        presentation
+      end
+
+      describe '#render' do
+        it 'knows how to create slide content from a Presentation' do
+          builder = SlidesHtmlBuilder.new presentation: presentation
+          expect(builder.render).to include "<section>"
+          expect(builder.render).to include "<blockquote"
+          expect(builder.render).to include "Hello Ruby!"
+        end
+      end
+
+    end
+
+    describe '#write_to' do
+
+      it 'can write #rendered output to a file' do
+        input_file = File.join data, 'slides.haml'
+        builder = SlidesHtmlBuilder.new input_file: input_file
+        tmp_file = Tempfile.new 'slides.html'
+        builder.write_to file: tmp_file.path
+
+        written_content = File.open(tmp_file).read
+        expect(written_content).to include "<section>"
+        expect(written_content).to include "<h1>"
+        expect(written_content).to include "Hello Haml!"
+      end
+
+    end
+  end
+end
