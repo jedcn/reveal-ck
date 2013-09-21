c2c07404d47e582cc4248d73dcda40a7d6696a9d
Jed Northridge <northridge@gmail.com>
2013-09-21 07:20:22 -0400

    [cane, reek] Tweaks for clarity

--- lib/reveal-ck/slide_builder.rb
+++ lib/reveal-ck/slide_builder.rb
@@ -25,40 +25,44 @@ module RevealCK
 
       add_task "Slicing out reveal.js default slides",
                lambda {
-                 line_nums = {
-                   default_slides: {
-                     first: 38, # Line where I see <div class="slides">
-                     last: 346  # Closing <div>
-                   }
-                 }
-                 default_slides = line_nums[:default_slides][:first]..line_nums[:default_slides][:last]
+                 begin_line_num = 38 # Line where I see <div class="slides">
+                 end_line_num = 346 # Closing <div>
+                 default_slides = begin_line_num..end_line_num
                  FileSlicer.remove! reveal_slides, default_slides
                }
 
       add_task "Splicing in slides from #{user_slides}",
                lambda {
-                 FileSplicer.insert! user_slides, into: reveal_slides, after: '<div class="slides">'
+                 FileSplicer.insert!(user_slides,
+                                     into: reveal_slides,
+                                     after: '<div class="slides">')
                }
 
       old_title = 'reveal.js - The HTML Presentation Framework'
       new_title = config.title
       add_task "Replacing the <title>",
                lambda {
-                 FileStringReplacer.replace! reveal_slides, old: old_title, new: config.title
+                 FileStringReplacer.replace!(reveal_slides,
+                                             old: old_title,
+                                             new: new_title)
                }
 
       old_author = 'name="author" content="Hakim El Hattab"'
       new_author = 'name="author" content="' + config.author + '"'
       add_task "Replacing the <meta name='author'>",
                lambda {
-                 FileStringReplacer.replace! reveal_slides, old: old_author, new: new_author
+                 FileStringReplacer.replace!(reveal_slides,
+                                             old: old_author,
+                                             new: new_author)
                }
 
       old_theme = 'href="css/theme/default.css" id="theme"'
       new_theme = 'href="css/theme/' + config.theme + '.css" id="theme"'
       add_task 'Replacing the core theme',
                lambda {
-                 FileStringReplacer.replace! reveal_slides, old: old_theme, new: new_theme
+                 FileStringReplacer.replace!(reveal_slides,
+                                             old: old_theme,
+                                             new: new_theme)
                }
     end
 
