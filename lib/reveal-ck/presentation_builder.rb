641a5e1c420b9b9cb7d6838777c57471fdb745ce
Jed Northridge <northridge@gmail.com>
2013-09-21 07:15:06 -0400

    [cane, reek] Tweaks for clarity

--- lib/reveal-ck/presentation_builder.rb
+++ lib/reveal-ck/presentation_builder.rb
@@ -14,7 +14,8 @@ module RevealCK
       @image_files = args[:image_files]
       @slides_file = args[:slides_file]
       @presentation = args[:presentation]
-      raise 'either :slides_file or :presentation is required' unless @slides_file || @presentation
+      missing_info = 'either :slides_file or :presentation is required'
+      raise missing_info unless @slides_file || @presentation
       @output_dir = args[:output_dir]
       @config = args[:config]
     end
@@ -38,40 +39,45 @@ module RevealCK
                  FileUtils.mkdir_p output_dir, verbose: false
                }
 
+      slides_html = output_dir 'slides.html'
       if @slides_file
-        add_task "Transforming #{slides_file} into #{output_dir('slides.html')}'}",
-                 lambda {
-                   builder = SlidesHtmlBuilder.new input_file: @slides_file
-                   builder.write_to file: output_dir('slides.html')
-                 }
+        action = "Transforming #{slides_file} into '#{slides_html}'}"
+        builder = SlidesHtmlBuilder.new input_file: @slides_file
       else
-        add_task "Transforming Presentation into #{output_dir('slides.html')}'}",
-                 lambda {
-                   builder = SlidesHtmlBuilder.new presentation: @presentation
-                   builder.write_to file: output_dir('slides.html')
-                 }
+        action = "Transforming Presentation into '#{slides_html}'}"
+        builder = SlidesHtmlBuilder.new presentation: @presentation
       end
+
+      add_task(action,
+               lambda {
+                 builder.write_to file: slides_html
+               })
 
       add_task "Bundling up the revealjs stuff into #{output_dir}/",
                lambda {
-                 FileUtils.cp_r RevealCK::REVEALJS_FILES, output_dir, verbose: false
+                   FileUtils.cp_r RevealCK::REVEALJS_FILES,
+                                output_dir,
+                                  verbose: false
                }
 
       if image_files
         add_task "Copying in images into #{output_dir('images')}",
                  lambda {
                    FileUtils.mkdir_p output_dir('images'), verbose: false
-                   FileUtils.cp_r image_files, output_dir('images'), verbose: false
+                   FileUtils.cp_r image_files,
+                                  output_dir('images'),
+                                  verbose: false
                  }
       end
 
       add_task "Creating slides/index.html",
                lambda {
-                 slide_builder = SlideBuilder.new({
-                                                    user_slides: output_dir('slides.html'),
-                                                    reveal_slides: output_dir('index.html'),
-                                                    config: @presentation || config
-                                                  })
+                 slide_builder =
+                   SlideBuilder.new({
+                                      user_slides: slides_html,
+                                      reveal_slides: output_dir('index.html'),
+                                      config: @presentation || config
+                                    })
                  slide_builder.build!
                }
 
