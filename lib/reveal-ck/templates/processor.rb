93949352a5a4646774198cf94d507a19418cbabf
Jed Northridge <northridge@gmail.com>
2013-09-27 06:55:26 -0400

    [whitespace] Somehow I added an extra space..

--- lib/reveal-ck/templates/processor.rb
+++ lib/reveal-ck/templates/processor.rb
@@ -1,32 +1,32 @@
- #
- # Setup Slim
- require 'slim'
- ::Slim::Engine.set_default_options pretty: true
+#
+# Setup Slim
+require 'slim'
+::Slim::Engine.set_default_options pretty: true
 
- module RevealCK
-   module Templates
-     #
-     # Public: A Processor is given a template and expected to render
-     # it.
-     #
-     class Processor
+module RevealCK
+  module Templates
+    #
+    # Public: A Processor is given a template and expected to render
+    # it.
+    #
+    class Processor
 
-       require 'tilt'
+      require 'tilt'
 
-       def initialize(file)
-         @template = Tilt.new file
-       end
+      def initialize(file)
+        @template = Tilt.new file
+      end
 
-       def output(scope = nil, locals = {})
-         scope = RenderScope.new
-         @template.render scope, locals
-       end
+      def output(scope = nil, locals = {})
+        scope = RenderScope.new
+        @template.render scope, locals
+      end
 
-       def self.open(file)
-         Processor.new file
-       end
+      def self.open(file)
+        Processor.new file
+      end
 
-     end
+    end
 
-   end
- end
+  end
+end
