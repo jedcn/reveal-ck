f835d19e3f051691121ae1ab7e50f823f779acfd
Jed Northridge <northridge@gmail.com>
2013-09-29 07:17:55 -0400

    [config-and-dsl] SlidesHtml is no longer needed

--- lib/reveal-ck/builders.rb
+++ lib/reveal-ck/builders.rb
@@ -1,5 +1,4 @@
 require_relative 'builders/build_task'
 require_relative 'builders/builder'
-require_relative 'builders/slides_html'
 require_relative 'builders/index_html'
 require_relative 'builders/packaged_slides'
