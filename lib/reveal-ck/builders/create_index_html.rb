f5f1eba255428d084b7d39392f554108cf0a2a0a
Jed Northridge <northridge@gmail.com>
2014-05-02 07:03:24 -0400

    [index.html.erb] Rewrote IndexHtml from an erb approach
    Previously, it was unpacking whatever reveal.js came with, and
    transforming that into the presentation of the user.
    
    This came from the mindset where I thought I'd be upgraded more
    frequently, and I wanted to "transform whatever reveal.js' index" looked
    like into the final result.
    
    No more-- with this approach we'll explicitly maintain an index.html.erb
    that may need to be updated each time the embedded reveal.js is
    embedded. But-- the thinking is that this is just fine because it
    doesn't happen very often and reveal.js isn't changing very much.

--- lib/reveal-ck/builders/create_index_html.rb
+++ lib/reveal-ck/builders/create_index_html.rb
@@ -9,11 +9,12 @@ module RevealCK
       include RequiredArg
       include RakeAware
 
-      attr_reader :slides_html, :output_dir, :config
+      attr_reader :slides_html, :index_html, :output_dir, :config
       attr_reader :application
 
       def initialize(args)
         @slides_file = retrieve(:slides_html, args)
+        @index_html = retrieve(:index_html, args)
         @output_dir = retrieve(:output_dir, args)
         @application = retrieve(:application, args)
         @config = retrieve(:config, args)
@@ -27,9 +28,12 @@ module RevealCK
       def setup
         index_html_file = "#{output_dir}/index.html"
         task(index_html_file => 'slides/slides.html') do
-          IndexHtml.new(user_slides: "#{output_dir}/slides.html",
-                        reveal_slides: "#{output_dir}/index.html",
-                        config: config).build!
+          content = IndexHtml.new(slides_file: "#{output_dir}/slides.html",
+                                  index_file: index_html,
+                                  config: config).render
+          File.open(index_html_file, 'w') do |index_html|
+            index_html.puts(content)
+          end
         end
         task(name => index_html_file)
       end
