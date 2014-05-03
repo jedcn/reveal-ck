dccefcba82c8af01f98e2c2bbf553ebaab49b8b8
Jed Northridge <northridge@gmail.com>
2014-05-02 21:50:35 -0400

    [index.html.erb] Converted CreateIndexHtml to CreationTask

--- lib/reveal-ck/builders/create_index_html.rb
+++ lib/reveal-ck/builders/create_index_html.rb
@@ -5,19 +5,15 @@ module RevealCK
     # Given a slides.html, a Rake application, and a place where the
     # final index.html file should end up, this class knows how to
     # work with Rake and create the index.html.
-    class CreateIndexHtml
-      include RequiredArg
-      include RakeAware
-
+    class CreateIndexHtml < CreationTask
       attr_reader :slides_html, :index_html, :output_dir, :config
-      attr_reader :application
 
       def initialize(args)
         @slides_file = retrieve(:slides_html, args)
         @index_html = retrieve(:index_html, args)
         @output_dir = retrieve(:output_dir, args)
-        @application = retrieve(:application, args)
         @config = retrieve(:config, args)
+        super
         setup
       end
 
@@ -35,7 +31,7 @@ module RevealCK
             index_html.puts(content)
           end
         end
-        task(name => index_html_file)
+        things_to_create.add(index_html_file)
       end
     end
   end
