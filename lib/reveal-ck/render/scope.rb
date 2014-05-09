a8429129a1a1a4224b173a357898c985b464d3aa
Jed Northridge <northridge@gmail.com>
2014-05-08 21:38:17 -0400

    [index-html-split] Extracted index_body.html.erb from index.html.erb

--- lib/reveal-ck/render/scope.rb
+++ lib/reveal-ck/render/scope.rb
@@ -18,6 +18,17 @@ module RevealCK
         template = Tilt.new(index_head_html_erb)
         template.render(self)
       end
+
+      def index_html_body(slides_file)
+        index_body_html_erb = RevealCK.path_to('templates',
+                                               'reveal.js',
+                                               'index_body.html.erb')
+        template = Tilt.new(index_body_html_erb)
+        locals = {
+          slides_file: slides_file
+        }
+        template.render(self, locals)
+      end
     end
   end
 end
