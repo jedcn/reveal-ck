373ec881982220708b132cb429314ef3f5ea2a1d
Jed Northridge <northridge@gmail.com>
2014-09-27 16:30:46 -0400

    [separate-triple-dash] Started vertically separating '---'
    There's now a pre-processing step when running markdown, and this
    ensures that the symbol reveal-ck uses for separating slides (---) is
    vertically separated from other content.
    
    In particular, this vertical separation ensures that the --- is not
    misinterpreted as a "setext" style heading.

--- lib/reveal-ck/markdown/slide_markdown.rb
+++ lib/reveal-ck/markdown/slide_markdown.rb
@@ -7,9 +7,18 @@ module RevealCK
       HR = '<hr>'
       HR_NEWLINE = "<hr>\n"
 
-      SLIDE_START = '<section>'
-      SLIDE_END = '</section>'
-      SLIDE_DIVIDER = "#{SLIDE_END}\n#{SLIDE_START}"
+      HTML_SLIDE_START = '<section>'
+      HTML_SLIDE_END = '</section>'
+      HTML_SLIDE_DIVIDER = "#{HTML_SLIDE_END}\n#{HTML_SLIDE_START}"
+
+      MARKDOWN_SLIDE_DIVIDER = '---'
+      MARKDOWN_SLIDE_DIVIDER_REGEX = /^---$/
+      MARKDOWN_SLIDE_DIVIDER_WITH_NEWLINES = "\n---\n"
+
+      def preprocess(doc)
+        doc.gsub(MARKDOWN_SLIDE_DIVIDER_REGEX,
+                 MARKDOWN_SLIDE_DIVIDER_WITH_NEWLINES)
+      end
 
       def postprocess(doc)
         doc = doc[HR.size, doc.size - 1] if doc.start_with?(HR)
@@ -17,8 +26,8 @@ module RevealCK
         if doc.end_with?(HR_NEWLINE)
           doc = doc[0, doc.size - 1 - HR_NEWLINE.size]
         end
-        doc = doc.gsub(HR, SLIDE_DIVIDER)
-        "#{SLIDE_START}\n#{doc}\n#{SLIDE_END}"
+        doc = doc.gsub(HR, HTML_SLIDE_DIVIDER)
+        "#{HTML_SLIDE_START}\n#{doc}\n#{HTML_SLIDE_END}"
       end
 
       def block_code(code, language)
