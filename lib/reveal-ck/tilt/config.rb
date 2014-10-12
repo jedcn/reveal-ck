47184b271c423795161120075cab325828fe143b
Jed Northridge <northridge@gmail.com>
2014-10-12 08:35:35 -0400

    [livereload] Added explicit requires for tilt adapters

--- lib/reveal-ck/tilt/config.rb
+++ lib/reveal-ck/tilt/config.rb
@@ -1,2 +1,12 @@
 # Direct Tilt to use SlideMarkdownTemplate for "md" files
 Tilt.register RevealCK::Markdown::SlideMarkdownTemplate, 'md'
+
+# The following requires are present to avoid warnings such as:
+#
+# WARN: tilt autoloading 'tilt/erb' in a non thread-safe way; explicit
+# require 'tilt/erb' suggested.
+#
+# The same goes for tilt/haml, tilt/redcloth
+require 'tilt/erb'
+require 'tilt/haml'
+require 'tilt/redcloth'
