fea1ca8fa0b64902127b57bc7cd00a5a617db05c
Jed Northridge <northridge@gmail.com>
2014-10-12 10:10:42 -0400

    [livereload] Increase all Aruba timeouts
    ..Doing this to mitigate travis-ci build failures via timeout. I thought
    this would only happen while testing "reveal-ck serve", but I'm seeing
    timeouts in "reveal-ck generate" as well.

--- features/support/env.rb
+++ features/support/env.rb
@@ -1,11 +1,6 @@
 require 'aruba/cucumber'
 
-Before('@slow') do
+Before do
   @aruba_io_wait_seconds = 10
   @aruba_timeout_seconds = 10
-end
-
-After('@slow') do
-  @aruba_io_wait_seconds = nil
-  @aruba_timeout_seconds = nil
 end
