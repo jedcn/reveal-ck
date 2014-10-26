4a175e053083aeab811b26daa753f6ead34008db
Jed Northridge <northridge@gmail.com>
2014-10-26 13:18:36 -0400

    [vertical-markdown] Changed up pre/post process algorithm
    Previously, we'd let the markdown processor take "---" and create an
    "<hr/>" out of it.. and then we'd go to work with what was left.
    
    Now, we explicitly seek out "---" and switch it to something the
    markdown processor won't touch and doesn't care about-- in the
    preprocessing stage: a boring old <div/>.
    
    This is nicer.. but it's happening because I'm planning to use "***" as
    the separator for vertical slides, and both --- and *** will end up as
    "<hr/>"s.
    
    If that happens-- I won't know which is which in the postproces step.

--- spec/lib/reveal-ck/markdown/slide_markdown_spec.rb
+++ spec/lib/reveal-ck/markdown/slide_markdown_spec.rb
@@ -8,68 +8,134 @@ module RevealCK
         SlideMarkdown.new
       end
 
+      let :three_slides_in_markdown do
+        <<-eos
+First
+---
+Second
+---
+Third
+eos
+      end
+
+      let :three_slides_after_preprocess do
+        <<-eos
+<div>DIVIDER</div>
+
+First
+
+<div>DIVIDER</div>
+
+Second
+
+<div>DIVIDER</div>
+
+Third
+
+<div>DIVIDER</div>
+eos
+      end
+
+      let :three_slides_after_postprocess do
+        <<-eos
+<section>
+
+<p>First</p>
+
+</section>
+<section>
+
+<p>Second</p>
+
+</section>
+<section>
+
+<p>Third</p>
+
+</section>
+eos
+      end
+
+      let :preprocess_standard_result do
+        <<-eos
+<div>DIVIDER</div>
+
+First
+
+<div>DIVIDER</div>
+eos
+      end
+
+      describe '#preprocess' do
+
+        it 'wraps a document with <div>DIVIDER</div>s' do
+          output = slide_markdown.preprocess 'First'
+          expect(output).to eq preprocess_standard_result
+        end
+
+        it 'is consistent when starting+ending separators are used' do
+          output = slide_markdown.preprocess <<-eos
+---
+First
+---
+eos
+          expect(output).to eq preprocess_standard_result
+        end
+
+        it 'is consistent when only starting separators are used' do
+          output = slide_markdown.preprocess <<-eos
+---
+First
+eos
+          expect(output).to eq preprocess_standard_result
+        end
+
+        it 'is consistent when only ending separators are used' do
+          output = slide_markdown.preprocess <<-eos
+First
+---
+eos
+          expect(output).to eq preprocess_standard_result
+        end
+
+        it 'can handle three slides' do
+          output = slide_markdown.preprocess three_slides_in_markdown
+          expect(output).to eq three_slides_after_preprocess
+        end
+      end
+
       describe '#postprocess' do
-        let :basic_input do
+        let :three_slide_input do
           <<-eos
-<h1>h1 Slide</h1>
+<div>DIVIDER</div>
 
-<hr>
+<p>First</p>
 
-<h2>h2 Slide</h2>
+<div>DIVIDER</div>
+
+<p>Second</p>
+
+<div>DIVIDER</div>
+
+<p>Third</p>
+
+<div>DIVIDER</div>
 eos
         end
 
-        let :leading_hr_input do
-          <<-eos
-<hr>
-
-<h1>h1 Slide</h1>
-
-<hr>
-
-<h2>h2 Slide</h2>
-eos
+        it 'starts the output with a <section> and a newline' do
+          output = slide_markdown.postprocess(three_slide_input)
+          expect(output).to start_with "<section>\n"
         end
 
-        let :trailing_hr_input do
-          <<-eos
-<h1>h1 Slide</h1>
-
-<hr>
-
-<h2>h2 Slide</h2>
-
-<hr>
-eos
+        it 'ends the output with a newline, a </section>, and a newline' do
+          output = slide_markdown.postprocess(three_slide_input)
+          expect(output).to end_with "\n</section>\n"
         end
 
-        it 'wraps its content in a <section>"' do
-          output = slide_markdown.postprocess(basic_input)
-          expect(output).to start_with '<section>'
-          expect(output).to end_with '</section>'
-          expect(output.scan('<section>').size).to eq 2
-          expect(output.scan('</section>').size).to eq 2
-        end
-
-        it 'replaces <hr> with section breaks' do
-          output = slide_markdown.postprocess(basic_input)
-          expect(output).to_not include 'hr'
-          expect(output).to include '<h1>h1 Slide</h1>'
-          expect(output).to include '<h2>h2 Slide</h2>'
-        end
-
-        it 'does not turn an initial <hr> into a section' do
-          output = slide_markdown.postprocess(leading_hr_input)
-          expect(output.scan('<section>').size).to eq 2
-          expect(output.scan('</section>').size).to eq 2
-          expect(output).to_not include 'hr'
-        end
-
-        it 'does not turn a trailing <hr> into a section' do
-          output = slide_markdown.postprocess(trailing_hr_input)
-          expect(output.scan('<section>').size).to eq 2
-          expect(output.scan('</section>').size).to eq 2
-          expect(output).to_not include 'hr'
+        it 'replaces intermediate <div>DIVIDER</div>s with section breaks' do
+          output = slide_markdown.postprocess(three_slide_input)
+          expect(output).to eq three_slides_after_postprocess
         end
       end
     end
