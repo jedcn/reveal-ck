b96ebb5dab92284f3f2474cb32933d0864c52ca7
Jed Northridge <git@jedcn.com>
2015-03-01 06:36:43 -0500

    [dependencies] Disable pry in Guard. Wake up every second.
    Adding no_interactions:true makes it so that guard goes to sleep
    immediately.
    
    This change introduces a thread that continually wakes up guard. If
    there are changes, guard does the work, if not, it goes back to sleep.

--- lib/reveal-ck/commands/listen_to_reload_browser.rb
+++ lib/reveal-ck/commands/listen_to_reload_browser.rb
@@ -10,11 +10,27 @@ module RevealCK
       end
 
       def run
+        sleeping_guard = setup_and_run_guard
+        regularly_wakeup sleeping_guard
+      end
+
+      private
+
+      def setup_and_run_guard
         require 'guard/cli'
         Guard::UI.options[:template] = "#{prefix} :message"
         guardfile = RevealCK.path('files/reveal-ck/Guardfile')
         Thread.new do
-          Guard.start(guardfile: guardfile)
+          Guard.start(guardfile: guardfile, no_interactions: true)
+        end
+      end
+
+      def regularly_wakeup(thread)
+        Thread.new do
+          loop do
+            sleep 1
+            thread.wakeup
+          end
         end
       end
     end
