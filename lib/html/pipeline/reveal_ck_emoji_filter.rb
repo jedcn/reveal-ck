912c1ba787117d9ae2f00a22857bcc2512ce5351
Jed Northridge <git@jedcn.com>
2015-09-27 21:29:17 -0400

    [emoji-filter] Override emoji_image_tag
    This is nicer because I want to use everything that EmojiFilter
    does *except* the image tag generation.
    
    The prior implementation had to get involved with finding emojis and
    gsubbing them out.
    
    This possible now because of @kbrock's change over here:
    
    https://github.com/jch/html-pipeline/pull/195/

--- lib/html/pipeline/reveal_ck_emoji_filter.rb
+++ lib/html/pipeline/reveal_ck_emoji_filter.rb
@@ -4,13 +4,8 @@ module HTML
     # width/height/align attributes so that reveal-ck supplied CSS
     # takes effect.
     class RevealCKEmojiFilter < EmojiFilter
-      def emoji_image_filter(text)
-        return text unless text.include?(':')
-
-        text.gsub(emoji_pattern) do
-          name = Regexp.last_match[1]
-          "<img class='emoji' alt=':#{name}:' src='#{emoji_url(name)}' />"
-        end
+      def emoji_image_tag(name)
+        "<img class='emoji' alt=':#{name}:' src='#{emoji_url(name)}' />"
       end
     end
   end
