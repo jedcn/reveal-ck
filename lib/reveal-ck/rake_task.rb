b27d875f5c5e671ad0dc41746b2adf9f4297f5b9
Jed Northridge <git@jedcn.com>
2016-03-09 18:52:57 -0500

    [doc-refresh] rubocop: block stuff
    First:
    
    lib/reveal-ck/rake_task.rb:64:13: C: Use yield instead of task_block.call.
        task_block.call(*[self, task_args].slice(0, task_block.arity))
                ^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^
    
    And then:
    
    lib/reveal-ck/rake_task.rb:63:11: C: Favor modifier if usage when having
    a single-line body. Another good alternative is the usage of control
    flow &&/||.

--- lib/reveal-ck/rake_task.rb
+++ lib/reveal-ck/rake_task.rb
@@ -60,9 +60,7 @@ module RevealCK
       desc 'Generate a reveal.js presentation' unless last_comment
       task name, *args do |_, task_args|
         RakeFileUtils.__send__(:verbose, verbose) do
-          if task_block
-            task_block.call(*[self, task_args].slice(0, task_block.arity))
-          end
+          yield(*[self, task_args].slice(0, task_block.arity)) if task_block
           run_task verbose
         end
       end
