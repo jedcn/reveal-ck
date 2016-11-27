09dc009ea8b0a83c80bcebf357117f4bf3020ace
Jed Northridge <git@jedcn.com>
2016-11-26 22:39:05 -0500

    [update-codeclimate] Updates for new version
    The gem that changed is called codeclimate-test-reporter.
    
    The way I was doing it before was for <1.0, and there have been breaking
    changes.
    
    See also:
    
    * https://github.com/codeclimate/ruby-test-reporter
    * https://docs.codeclimate.com/v1.0/docs/travis-ci-ruby-test-coverage

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,11 +1,6 @@
 require 'simplecov'
 require 'codeclimate-test-reporter'
-SimpleCov.start do
-  formatter SimpleCov::Formatter::MultiFormatter[
-    SimpleCov::Formatter::HTMLFormatter,
-    CodeClimate::TestReporter::Formatter
-  ]
-end
+SimpleCov.start
 
 # silence tilt autoload warnings
 require 'tilt/erb'
